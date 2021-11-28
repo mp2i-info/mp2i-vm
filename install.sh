2bd91dbd46ce412ee88d3d99f048d9d643d2fec0
Quentin Fortier <qpfortier@gmail.com>
2021-11-28 11:26:45 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # general tools
 sudo apt-get update && apt install -y software-properties-common && add-apt-repository ppa:avsm/ppa \
-&& apt install -y --no-install-recommends zlib1g-dev libffi-dev libgmp-dev libzmq5-dev pkg-config build-essential curl sudo 
+&& apt install -y --no-install-recommends zlib1g-dev libffi-dev libgmp-dev libzmq5-dev pkg-config build-essential 
 
 # ocaml
 sudo apt install -y ocaml opam python3-pip
